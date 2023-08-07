9b0f5bd69d02e5bf34f63c6c3d455ea6690569a1
Fabian Georgi <fabian.georgi@student.hpi.uni-potsdam.de>
2023-08-07 19:59:50 +0200

    make backlink atomic again

--- src/skip_list.hpp
+++ src/skip_list.hpp
@@ -93,7 +93,7 @@ struct Node {
     // Value
     Element element;
     // Pointer to the previous Node
-    Node* backLink;
+    std::atomic<Node*> backLink;
     // Stores next node (right), and if node is marked OR flagged
     std::atomic<Successor> successor;
     // A pointer to the node below, or null if root node (lowest level)
