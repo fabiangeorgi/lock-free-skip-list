ddcf35c115506e6e941ae383f3bdb0390215038d
Fabian Georgi <fabian.georgi@student.hpi.uni-potsdam.de>
2023-08-08 12:47:48 +0200

    check if that let's it fail

--- src/skip_list.cpp
+++ src/skip_list.cpp
@@ -25,6 +25,9 @@ SkipList::SkipList() {
     }
     // In the paper it states that top head should reference itself with up, but is not needed
     // iteratorHead->up = iteratorHead;
+
+    head->down = head;
+    tail->down = tail;
 }
 
 bool SkipList::insert(Key key, Element element) {
