bc71651503a3daeaa2dffb49fcfaae0470743152
Fabian Georgi <fabian.georgi@student.hpi.uni-potsdam.de>
2023-08-12 23:15:21 +0200

    remove thread_local from insertionMap

--- src/skip_list.cpp
+++ src/skip_list.cpp
@@ -32,7 +32,7 @@ SkipList::SkipList() {
  * Insert new Node/Tower into Skip List
  */
 bool SkipList::insert(Key key, Element element) {
-    thread_local std::map<Level, std::pair<Node*, Node*>> insertionMemory{};
+    std::map<Level, std::pair<Node*, Node*>> insertionMemory{};
 
     Node *prevNode;
     Node *nextNode;
