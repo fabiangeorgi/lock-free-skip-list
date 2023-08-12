ea6041ac2f89a21654302973bbf9d10f4470bdc2
Fabian Georgi <fabian.georgi@student.hpi.uni-potsdam.de>
2023-08-12 23:33:23 +0200

    test this

--- src/skip_list.cpp
+++ src/skip_list.cpp
@@ -93,7 +93,7 @@ bool SkipList::insert(Key key, Element element) {
         if (insertionMemory.contains(currV)) {
             std::tie(prevNode, nextNode) = insertionMemory[currV];
         } else {
-            std::tie(prevNode, nextNode) = searchToLevel(key, currV);
+            std::tie(prevNode, nextNode) = searchToLevelWithInsertionMap(key, currV, insertionMemory);
         }
 
     }
